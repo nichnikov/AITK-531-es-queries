2dbadcbb3ace27e2834cbcda86b1c4064e5d1bf8
nichnikov <a.nichnikov@gmail.com>
2024-05-16 12:12:07 +0300

    ref

--- query_construct.py
+++ query_construct.py
@@ -2,7 +2,11 @@ from queries import Bool, Match
 
 tokens_str = "мой вопрос такой потому что"
 pub_id = 5
-test_query=  Bool([Match("LemCluster", tokens_str), Match("ParentPubList", pub_id)])
+test_query = Bool([Match("LemCluster", tokens_str), Match("ParentPubList", pub_id)])
+
 print(test_query)
 print(type(test_query))
 print(test_query.to_dict())
+
+test_query2 = Bool([Match("LemCluster", tokens_str)])
+print(test_query2.to_dict())
